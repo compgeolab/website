b55457b9d5cba1c6847b635368d86b362e4d72c2
Leonardo Uieda <leouieda@gmail.com>
2020-07-30 21:48:40 +0100

    Find all md files recursively

--- _python/serve.py
+++ _python/serve.py
@@ -5,7 +5,7 @@ from livereload import Server, shell
 
 
 server = Server()
-files = ["**.md", "_site.yml", "images/", "css/", "js/", "_layouts/", "_python/"]
+files = ["**/**.md", "_site.yml", "images/", "css/", "js/", "_layouts/", "_python/"]
 for filename in files:
     server.watch(filename, "urubu build")
 server.serve(root="_build", port="8008", host="localhost", open_url_delay=1)
